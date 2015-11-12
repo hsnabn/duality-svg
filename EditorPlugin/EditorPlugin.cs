80da1a052b475660b2afb3b956204ee4c4d5857f
Aban Hussain <hsnabn@gmail.com>
2015-11-12 12:24:53 +0500

    Initial commit.

---
+++ EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace DualitySVG.Editor
+{
+    // Defines a Duality editor plugin.
+    public class DualitySVGEditorPlugin : EditorPlugin
+    {
+        public override string Id
+        {
+            get { return "DualitySVGEditorPlugin"; }
+        }
+    }
+}
